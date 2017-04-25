84b70d4d385e74a7568d6526e3e39376b0876ff3
anxu <axu.home@gmail.com>
2017-04-25 16:37:18 +0800

    CSV文件读取添加设置

--- daily_put/reindex.py
+++ daily_put/reindex.py
@@ -1,25 +1,13 @@
 # coding: utf-8
 
-"""
-[功能说明]
-该脚本的功能是可以根据自定义规则，重写`csv`格式文件
-可以通过`fieldnames`参数指定`csv`文件列名
-重写规则在`reset_row(row, in_filename, out_filename)`方法中
-  - row: 读到的当前行的值，若制定了`fieldnames`参数则为`dict`。若没有指定`fieldnames`参数则为`list`顺序和`csv`文件顺序一致。
-  - in_filename: 输入文件名
-  - out_filename: 输出文件名
-
-[使用方法]
-python reindex.py ${in_filename} ${out_filename}
-"""
-
 import csv
 import sys
+import os
 from datetime import datetime
 
 # 默认列名称（共38列）
 DEFAULT_FIELDNAMES = ["filename", "area_of_job", "start_time", "end_time", "duration", "callnumber", "rep_no", "rep_group", "agent_no", "account_no", "rep_team", "calltype", "number_of_record", "region", "project_list", "satisfaction", "qa_no", "summarize_group", "summarize_class",
-                      "summarize_project", "summarize_content", "is_qa", "to_warehouse", "to_improvement", "isnormality", "type_of_service", "data_status", "data_status_2", "data_status_3", "group_leader", "cust_id", "cust_name", "audit_no", "phone_type", "verify_type", "staff_name", "bu_type", "rule_prompt"]
+                      "summarize_project", "summarize_content", "is_qa", "to_warehouse", "to_improvement", "isnormality", "type_of_service", "data_status", "data_status_2", "data_status_3", "group_leader", "cust_id", "cust_name", "audit_no", "phone_type", "verify_type", "staff_name", "bu_type", "rule_prompt", "field_01", "field_02", "field_03", "field_04", "field_05", "field_06", "field_07", "field_08", "field_09", "field_10", "field_11", "field_02", "field_13", "field_14"]
 
 
 def reset_index(in_filename, out_filename, fieldnames=None, is_write_headers=False, *args, **kwds):
@@ -34,10 +22,20 @@ def reset_index(in_filename, out_filename, fieldnames=None, is_write_headers=Fal
     """
 
     try:
+        csv.register_dialect(  
+           'mydialect',  
+           # delimiter = ',',  
+           quotechar = '\'',  
+           # doublequote = True,  
+           # skipinitialspace = True,  
+           # lineterminator = '\r\n',  
+           # quoting = csv.QUOTE_MINIMAL
+        )  
+
         # 打开读文件句柄
         with open(in_filename, 'rb') as in_f:
             reader = csv.reader(in_f) if fieldnames is None else csv.DictReader(
-                in_f, fieldnames=fieldnames)
+                in_f, fieldnames=fieldnames, dialect='mydialect')
 
             # 打开写文件句柄
             with open(out_filename, 'wb') as out_f:
@@ -49,9 +47,10 @@ def reset_index(in_filename, out_filename, fieldnames=None, is_write_headers=Fal
                     writer.writeheader()
 
                 # 读每行
-                for row in reader:
+                for row in reader:                
                     writer.writerow(reset_row(row, in_filename,
                                               out_filename, *args, **kwds))
+
     except Exception as e:
         print "Error Message: [{0}]".format(e)
         return False
@@ -78,9 +77,8 @@ def reset_row(row, in_filename, out_filename, *args, **kwds):
     date_format = "%Y-%m-%d %H:%M:%S"
 
     # === 处理说明
-    # out_filename -> "kf_kf01_20170322_1.index"
-    # out_filename.split("_")[2] -> "20170322"
-    dt = datetime.strptime(out_filename.split("_")[2], "%Y%m%d")
+    # out_filename -> "kf_20170322.index"
+    dt = datetime.strptime(os.path.basename(out_filename).split("_")[1].split(".")[0], "%Y%m%d")
 
     # === 处理说明
     # row["start_time"] -> "'2017-02-20 12:00:00'"
@@ -94,9 +92,13 @@ def reset_row(row, in_filename, out_filename, *args, **kwds):
     row["start_time"] = "'{0}'".format(start_time)
     row["end_time"] = "'{0}'".format(end_time)
 
+    # 重写 `filename`列
+    row["filename"] = "'{0}_{1}'".format(os.path.basename(out_filename).split("_")[1].split(".")[0], row["filename"].strip("''"))
+
     return row
 
 
 if __name__ == '__main__':
     _, i_file, o_file = sys.argv
     reset_index(i_file, o_file, fieldnames=DEFAULT_FIELDNAMES)
+
